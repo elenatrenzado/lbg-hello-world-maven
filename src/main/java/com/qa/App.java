34d191ef135048ed4ff966a7d932f96d7fbda2c6
unknown <eletrenzado@gmail.com>
2023-10-30 14:12:31 +0000

    add my name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
+        System.out.println(sayHelloToSomeone("Elena"));
     }
 
     public static String sayHello(){
